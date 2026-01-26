227bcb6724f9452587ab22e0c142ceef9ac79cb6
shaukat1388 <shaukat1388@gmail.com>
2026-01-25 23:03:58 -0600

    mnt

--- usermodule/testmodule/backend.tf
+++ usermodule/testmodule/backend.tf
@@ -1,9 +1,9 @@
 
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "ResourceGroup1"
-    storage_account_name = "storageaccount1cat"
-    container_name       = "scontainer1"
-    key                  = "multiusers.tfstate"
-  }
-}
+#terraform {
+#  backend "azurerm" {
+#    resource_group_name  = "ResourceGroup1"
+#    storage_account_name = "storageaccount1cat"
+#    container_name       = "scontainer1"
+#    key                  = "multiusers.tfstate"
+#  }
+#}
