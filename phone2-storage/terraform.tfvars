fb83bf52eb22a22ee3af132c0b6bac2c22615a06
shaukat1388 <shaukat1388@gmail.com>
2026-01-27 20:55:52 -0600

    read-only

--- phone2-storage/terraform.tfvars
+++ phone2-storage/terraform.tfvars
@@ -1,3 +1,3 @@
-resource_group_name    = "ResourceGroup2"
+resource_group_name    = "ResourceGroup12345"
 storage_account_name   = "phone2storage12345"
 phone2_password        = "StrongPassword@123"
--- singleuser-module/backend.tf
+++ singleuser-module/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "ResouceGroup1"
     storage_account_name = "storageaccount10cat"
     container_name       = "scontainer1"
-    key                  = "phone2-users.tfstate"
+    key                  = "singleuser.tfstate"
   }
 }
