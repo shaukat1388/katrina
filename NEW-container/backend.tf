cc599b203dfd83f36ad86da99d117173d0cc7b8c
shaukat1388 <shaukat1388@gmail.com>
2026-01-27 19:37:46 -0600

    NEW

--- NEW-container/backend.tf
+++ NEW-container/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "ResouceGroup1"
     storage_account_name = "storageaccount10cat"
-    container_name       = "NEW-container"
-    key                  = "NEW-container.tfstate"
+    container_name       = "newcontainer"
+    key                  = "new-container.tfstate"
   }
 }
