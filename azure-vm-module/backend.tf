d717a747b3e4a32f2cffec3bb7272473e282e7c1
shaukat1388 <shaukat1388@gmail.com>
2026-01-31 16:18:24 -0600

    vmmodule

--- azure-vm-module/backend.tf
+++ azure-vm-module/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "ResouceGroup1"
     storage_account_name = "storageaccount10cat"
     container_name       = "scontainer1"
-    key                  = "katrinavm2.tfstate"
+    key                  = "katrinavm3.tfstate"
   }
 }
