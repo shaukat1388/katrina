a69de97e4e7c76531fe311fc0272d8b494f1d9c8
shaukat1388 <shaukat1388@gmail.com>
2026-02-04 22:46:02 -0600

    docker

--- docker/backend.tf
+++ docker/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "ResouceGroup1"
     storage_account_name = "storageaccount10cat"
     container_name       = "scontainer1"
-    key                  = "docker1.tfstate"
+    key                  = "docker2.tfstate"
   }
 }
