cf7606db993fe44679f67e89a8505b81a361b768
shaukat1388 <shaukat1388@gmail.com>
2026-02-05 00:09:42 -0600

    docker

--- docker/main.tf
+++ docker/main.tf
@@ -1,6 +1,12 @@
-# Use existing resource group
+# Use existing Resource Group
 data "azurerm_resource_group" "this" {
   name = var.resource_group_name
+}
+
+# Reference your existing ACR
+data "azurerm_container_registry" "this" {
+  name                = "<YOUR_ACR_NAME>"   # <-- replace with your ACR name
+  resource_group_name = data.azurerm_resource_group.this.name
 }
 
 # Create Azure Container Instance
@@ -13,9 +19,16 @@ resource "azurerm_container_group" "this" {
   ip_address_type = "Public"
   dns_name_label  = var.dns_name_label
 
+  # Pull image from your private ACR
+  image_registry_credential {
+    server   = data.azurerm_container_registry.this.login_server
+    username = data.azurerm_container_registry.this.admin_username
+    password = data.azurerm_container_registry.this.admin_password
+  }
+
   container {
     name   = var.container_name
-    image  = "nginx:latest"   # <-- Quickstart image from Docker Hub
+    image  = "${data.azurerm_container_registry.this.login_server}/nginx:latest"  # <-- Quickstart image imported into ACR
     cpu    = var.cpu
     memory = var.memory
 
